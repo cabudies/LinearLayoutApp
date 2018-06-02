4fd85feea566716c36a7a770ef241c86fc4394de
Gurjas Singh <gurjassingh92@gmail.com>
2018-06-02 14:45:57 +0530

    1. New activity layout and java file added. 2. Intent function used to call other activity.

--- app/src/main/java/com/brillicaservices/gurjas/linearlayoutapp/MainActivity.java
+++ app/src/main/java/com/brillicaservices/gurjas/linearlayoutapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.brillicaservices.gurjas.linearlayoutapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /*
  * @Author: Gurjas Singh,
@@ -12,10 +14,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     /*
-     * Creating a Global variable of TextView.
-     * So that we can use it's reference in other functions as well.
-     * */
-    TextView obj;
+    * Create a new Button object*/
+    Button btn;
 
     /*
      * onCreate is default function that we need for every activity.
@@ -31,14 +31,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         /*
-         * Casting TextView object with it's ID.
-         * R.id.hello_world has been declared in the activity_main.xml file.
-         * */
-        obj = (TextView) findViewById(R.id.hello_world);
+        * assign button reference to the button object*/
+        btn = (Button) findViewById(R.id.next_activity_button);
 
         /*
-         * Using predefined function .setText() to set text to the text view
-         * */
-        obj.setText("Gurjas");
+        * Performing button @onClickListener()
+        * when you start writing 'new View.OnClickListener(), compiler will give you suggestion
+        * select it and it will automatically complete the whole code for you.'*/
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /*
+                * Use Intent class to class another activity
+                * inside Intent constructor, pass the current Activity and second activity*/
+                Intent intent = new Intent(MainActivity.this, NewActivity.class);
+
+                /*
+                * Use this method to call other activity*/
+                startActivity(intent);
+            }
+        });
     }
 }
